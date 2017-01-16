325d71bbb937e22e9ef1b490c0521dafff2f429e
Eric Davis <endavis@gmail.com>
2017-01-16 12:17:13 -0500

    changes for new variable names

--- NetApp/Cluster/checkthinprovisioned.py
+++ NetApp/Cluster/checkthinprovisioned.py
@@ -6,11 +6,11 @@ if __name__ == '__main__':
   args = naparser.parse_args()
   NApps = NAManager(args)
 
-  for host in NApps.netapps.values():
-    for svm in host.svms.values():
+  for cluster in NApps.clusters.values():
+    for svm in cluster.svms.values():
       svm.fetchvolumes()
 
       for volume in svm.volumes.values():
         if not ('_root' in volume.name) and volume.name != 'vol0':
           if volume.attr['Space Guarantee Style'] != 'none':
-            print('%s (%s) - Not thin - %-20s : %-40s' % (host.name, host.cname, svm.name, volume.name))
+            print('%s (%s) - Not thin - %-20s : %-40s' % (cluster.name, cluster.cname, svm.name, volume.name))
