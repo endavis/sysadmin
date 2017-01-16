5ef3579edbd74781d0916f2814a05ab47a198d3c
Eric Davis <endavis@gmail.com>
2017-01-16 11:30:12 -0500

    added command line argument for cluster to check
    will check all clusters in config file if no argument is passed in

--- NetApp/Cluster/checksnapvolsizes.py
+++ NetApp/Cluster/checksnapvolsizes.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-from NA import NAManager, naparser
+from NA import NAManager, naparser, approximate_size
 
-if __name__ == '__main__':
-  args = naparser.parse_args()
-  NApps = NAManager(args)
-  DAL = NApps.netapps['DAL']
-  DAL.fetchpeers()
-  DAL.fetchsnapmirrors()
 
-  for mirror in DAL.snapmirrors:
+def checkcluster(cluster):
+  cluster.fetchpeers()
+  cluster.fetchsnapmirrors()
+
+  for mirror in cluster.snapmirrors:
     srcsvm = mirror['Source Path']['svm']
     srcvol = mirror['Source Path']['vol']
     svol = NApps.findvolume(srcvol, svm=srcsvm)
@@ -17,8 +15,32 @@ if __name__ == '__main__':
     destvol = mirror['Destination Path']['vol']
     dvol = NApps.findvolume(destvol, svm=destsvm)
 
-    if svol.attr['Volume Size'] != dvol.attr['Volume Size']:
-      print('%s:%s:%s not equal to %s:%s:%s' % (srcsvm, srcvol, svol.attr['Volume Size'],
-                                             destsvm, destvol, dvol.attr['Volume Size']))
+    if not svol:
+      print('Could not find source volume - %s:%s' % (srcsvm, srcvol))
 
+    if not dvol:
+      print('Could not find destination volume - %s:%s' % (destsvm, destvol))
+
+    if not dvol or not svol:
+      continue
+
+    if svol.attr['Volume Size'] != dvol.attr['Volume Size']:
+      print('%s:%s:%s not equal to %s:%s:%s' % (srcsvm, srcvol, approximate_size(svol.attr['Volume Size'], False),
+                                            destsvm, destvol, approximate_size(dvol.attr['Volume Size'], False)))
+
+if __name__ == '__main__':
+  naparser.add_argument('-n', '--netapp',
+                  help='the cluster to check')
+  args = naparser.parse_args()
+  NApps = NAManager(args)
+
+  if args.netapp:
+    if args.netapp in NApps.netapps:
+      checkcluster(NApps.netapps[args.netapp])
+    else:
+      print('%s is not a valid Netapp. Valid Netapps in config are %s.' % (args.netapp, ", ".join(NApps.netapps.keys())))
+
+  else:
+    for cluster in NApps.netapps.values():
+      checkcluster(cluster)
 
