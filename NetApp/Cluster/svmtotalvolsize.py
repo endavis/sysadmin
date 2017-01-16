3abf6eb1431b5f244ff2cffe673b0aab4aa21792
Eric Davis <endavis@gmail.com>
2017-01-16 12:33:45 -0500

    add arguments for cluster as well as svm
    make some functions so code is not duplicated

--- NetApp/Cluster/svmtotalvolsize.py
+++ NetApp/Cluster/svmtotalvolsize.py
@@ -1,48 +1,65 @@
 #/usr/bin/env python3
+from NA import ClusterManager, approximate_size, naparser
 
-from NA import NAManager, approximate_size, naparser
+def checksvm(svm):
+  svm.fetchvolumes()
+  size = 0
+  for volume in svm.volumes.values():
+    if not ('_root' in volume.name):
+      if volume.attr['Volume State'] != 'offline':
+        try:
+          size = size + volume.attr['Used Size']
+        except TypeError:
+          for i in volume.attr:
+            print(i, volume.attr[i])
+
+  print('%s (%s) - Total size for volumes in %s: %s' % (svm.cluster.name, svm.cluster.cname, svm.name, approximate_size(size, False)))
+  return size
+
+def checkcluster(cluster):
+  totalsize = 0
+  for svm in cluster.svms.values():
+    size = checksvm(svm)
+
+    totalsize = totalsize + size
+
+  print('%s (%s) - Total size of used data: %s' % (cluster.name, cluster.cname, approximate_size(totalsize, False)))
 
 if __name__ == '__main__':
   naparser.add_argument('-s', '--svm',
                   help='the svm to total')
-  naparser.add_argument('-n', '--netapp',
+  naparser.add_argument('-cl', '--cluster',
                   help='the cluster of the svm, must be in the config')
 
   args = naparser.parse_args()
-  NApps = NAManager(args)
+  CLMan = ClusterManager(args)
 
-  if args.svm:
-    if args.netapp in NApps.netapps:
-      cluster = NApps.netapps[args.netapp]
+  if args.svm and args.cluster:
+    if args.cluster in CLMan.clusters:
+      cluster = CLMan.clusters[args.cluster]
 
       if args.svm in cluster.svms:
-        cluster.svms[args.svm].fetchvolumes()
-        size = 0
-        for volume in cluster.svms[args.svm].volumes.values():
-            if not ('_root' in volume.name):
-                size = size + volume.attr['Used Size']
-
-        print('%s - Total size for volumes in %s: %s' % (cluster.name, args.svm, approximate_size(size, False)))
+        checksvm(cluster.svms[args.svm])
       else:
         print('%s is not a valid svm' % args.svm)
-        print('valid svms for cluster %s:' % (args.netapp))
+        print('valid svms for cluster %s:' % (args.cluster))
         print('%s' % ", ".join(cluster.svms.keys()))
     else:
-      print('%s is not a valid netapp cluster, valid clusters in config file are: %s' % (args.netapp, ", ".join(NApps.netapps.keys())))
+      print('%s is not a valid netapp cluster, valid clusters in config file are: %s' % (args.cluster, ", ".join(CLMan.clusters.keys())))
+  elif args.cluster:
+    if args.cluster in CLMan.clusters:
+      checkcluster(CLMan.clusters[args.cluster])
+
+    else:
+      print('%s is not a valid netapp cluster, valid clusters in config file are: %s' % (args.cluster, ", ".join(CLMan.clusters.keys())))
+
+  elif args.svm:
+    svm = CLMan.findsvm(args.svm)
+    if svm:
+      checksvm(svm)
+    else:
+      print('%s is not a valid svm' % (args.svm))
+
   else:
-    for cluster in NApps.netapps.values():
-      totalsize = 0
-      for svm in cluster.svms.values():
-        svm.fetchvolumes()
-        size = 0
-        for volume in svm.volumes.values():
-          if not ('_root' in volume.name) and volume.name != 'vol0':
-            if volume.attr['Volume State'] != 'offline':
-              size = size + volume.attr['Used Size']
-
-        totalsize = totalsize + size
-
-        print('%s (%s) - Total size for volumes in %s: %s' % (cluster.name, cluster.cname, svm.name, approximate_size(size, False)))
-
-      print('%s (%s) - Total size of used data: %s' % (cluster.name, cluster.cname, approximate_size(totalsize, False)))
-
+    for cluster in CLMan.clusters.values():
+      checkcluster(cluster)
