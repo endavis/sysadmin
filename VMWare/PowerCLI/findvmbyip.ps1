b1a5f578f7a30a0addba1b7f5cc8c637a5453791
Eric Davis <endavis@gmail.com>
2017-02-09 11:58:50 -0500

    add script to find VMs by an ip

---
+++ VMWare/PowerCLI/findvmbyip.ps1
@@ -0,0 +1,12 @@
+﻿param( $server, $match_ip)
+
+Connect-VIServer -Server $server
+
+Get-VM | %{
+      $vmIPs = $_.Guest.IPAddress
+      foreach($ip in $vmIPs) {
+          if ($ip -eq $match_ip) {
+              "Found VM with matching address: {0}" -f $_.Name
+          }
+      }
+  }
