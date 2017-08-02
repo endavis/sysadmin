b02da23fbaadb471e4ccd2b8169845ca79831709
Eric Davis <endavis@gmail.com>
2017-08-02 16:35:08 -0400

    add functions to find vm by mac and ip

---
+++ VMWare/PowerCLI/functions/findvmbymac.ps1
@@ -0,0 +1,9 @@
+﻿
+function FindVMbyMac {
+    $vCenter = $args[0]
+    $match_mac = $args[1]
+
+    Connect-VCenter $vCenter
+
+    get-vm | Get-NetworkAdapter | where {$_.macaddress -eq $match_mac} | select parent, macaddress
+}
---
+++ VMWare/PowerCLI/functions/findvmbynetwork.ps1
@@ -0,0 +1,17 @@
+﻿
+function FindVMbyNet {
+    $vCenter = $args[0]
+    $matchnet = $args[1] # 10.183.137*
+
+    Connect-VCenter $vCenter
+
+    foreach($vm in (Get-VM)){
+  
+      $vmIP = $vm.Guest.IPAddress
+      foreach($ip in $vmIP){
+        if($ip -like $matchnet) {
+          ($vm.Name + ',' + $ip)
+        }
+      }
+    }
+}
