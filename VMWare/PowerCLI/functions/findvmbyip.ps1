3f424c54f5fb750a91c2ec6dc72dae9729ed7454
Eric Davis <endavis@gmail.com>
2017-08-02 16:28:38 -0400

    move it to functions

---
+++ VMWare/PowerCLI/functions/findvmbyip.ps1
@@ -0,0 +1,16 @@
+﻿
+#requires -Modules VMware.VimAutomation.Core
+
+param( $server, $match_ip)
+
+Connect-VIServer -Server $server
+
+Get-VM | %{
+      $vmIPs = $_.Guest.IPAddress
+      foreach($ip in $vmIPs) {
+          if ($ip -eq $match_ip) {
+              "Found VM with matching address: {0}" -f $_.Name
+          }
+      }
+  }
+
