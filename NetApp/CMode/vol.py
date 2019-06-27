7c49cf091331c962095b50ac99a3ba7d0a4cdb1b
Eric Davis <endavis@gmail.com>
2019-06-27 14:11:10 -0400

    add ability to specify single volume on command line add "action" to options that are actions

--- NetApp/CMode/vol.py
+++ NetApp/CMode/vol.py
@@ -34,72 +34,83 @@ if __name__ == '__main__':
   naparser.add_argument('-ch', '--check', action='store_true',
                   help='check input file for nonexistent volumes')
   naparser.add_argument('-i', '--input',
-                  help='the input file of volumes',
-                  default='volumes.txt')
+                  help='the input file of volumes')
+  naparser.add_argument('-v', '--volume',
+                  help='act on a specific volume of the form CTL:Volume')
   naparser.add_argument('-m', '--mount', action='store_true',
-                  help='mount the volumes')
+                  help='action: mount the volumes')
   naparser.add_argument('-u', '--unmount', action='store_true',
-                  help='unmount the volumes')
+                  help='action: unmount the volumes')
   naparser.add_argument('-off', '--offline', action='store_true',
-                  help='offline the volume')
+                  help='action: offline the volume')
   naparser.add_argument('-on', '--online', action='store_true',
-                  help='online the volume')
+                  help='action: online the volume')
   naparser.add_argument('--delete', action='store_true',
-                  help='delete the volumes')
+                  help='action: delete the volumes')
   naparser.add_argument('-sr', '--releasesnapmirror', action='store_true',
-                  help='release all snapmirror destinations')
+                  help='action: release all snapmirror destinations')
   naparser.add_argument('-sl', '--listsnapmirror', action='store_true',
-                  help='list all snapmirror destinations')
+                  help='action: list all snapmirror destinations')
   naparser.add_argument('-d', '--details', action='store_true',
-                  help='get details for volumes')
+                  help='action: get details for volumes')
   naparser.add_argument('-cl', '--clones', action='store_true',
-                  help='check if volumes has clones')
+                  help='action: check if volumes has clones')
 
   args = naparser.parse_args()
 
   if not (args.unmount or args.mount or args.offline or args.online or args.delete or args.details or args.releasesnapmirror or args.listsnapmirror or args.clones or args.check):
-    print('Please specifiy one of the options')
+    print('Please specify an action')
+    sys.exit(1)
+
+  if (not (args.volume or args.input)):
+    print('Please specify a volume, either through -i with an input file or specifically with -v')
     sys.exit(1)
 
   CLMan = ClusterManager(args)
 
-  volumes = read_file(args.input)
+  if args.input:
+    volumes = read_file(args.input)
+  elif args.volume:
+    volumes = []
+    cls, vol = args.volume.split(':')
+    volumes.append({'cluster':cls, 'vol':vol})
 
-  for volume in volumes:
-    vol = volume['vol']
-    cluster = volume['cluster']
-    vols = CLMan.findvolume(vol, cluster=cluster)
-    nvol = None
-    if len(vols) > 1:
-      print('More than one volume found with the name %s' % vol)
-      for vol in vols:
-        print(vol)
-      continue
-    elif len(vols) == 0:
-      print('%s - %s not found' % (cluster, vol))
-      continue
-    elif len(vols) == 1:
-      nvol = vols[0]
+  if volumes:
+    for volume in volumes:
+      vol = volume['vol']
+      cluster = volume['cluster']
+      vols = CLMan.findvolume(vol, cluster=cluster)
+      nvol = None
+      if len(vols) > 1:
+        print('More than one volume found with the name %s' % vol)
+        for vol in vols:
+          print(vol)
+        continue
+      elif len(vols) == 0:
+        print('%s - %s not found' % (cluster, vol))
+        continue
+      elif len(vols) == 1:
+        nvol = vols[0]
 
-    if nvol:
-      if args.check:
-        print('Found volume %s - %s - %s' % (cluster, nvol.svm.name, vol))
-      if args.unmount:
-        nvol.unmount()
-      if args.mount:
-        nvol.mount()
-      if args.offline:
-        nvol.offline()
-      if args.online:
-        nvol.online()
-      if args.delete:
-        nvol.delete()
-      if args.releasesnapmirror:
-        nvol.snapmirrorreleaseall()
-      if args.listsnapmirror:
-        listsnapmirror(nvol)
-      if args.clones:
-        nvol.checkclones()
-      if args.details:
-        print('%s,%s,%s,%s,%s,%s,%s' % (nvol.svm.cluster.name, nvol.svm.cluster.cname, nvol.svm.name, nvol.attr['Aggregate Name'], nvol.name, nvol.attr['Used Size'], nvol.attr['Total Physical Used Size']))
+      if nvol:
+        if args.check:
+          print('Found volume %s - %s - %s' % (cluster, nvol.svm.name, vol))
+        if args.unmount:
+          nvol.unmount()
+        if args.mount:
+          nvol.mount()
+        if args.offline:
+          nvol.offline()
+        if args.online:
+          nvol.online()
+        if args.delete:
+          nvol.delete()
+        if args.releasesnapmirror:
+          nvol.snapmirrorreleaseall()
+        if args.listsnapmirror:
+          listsnapmirror(nvol)
+        if args.clones:
+          nvol.checkclones()
+        if args.details:
+          print('%s,%s,%s,%s,%s,%s,%s' % (nvol.svm.cluster.name, nvol.svm.cluster.cname, nvol.svm.name, nvol.attr['Aggregate Name'], nvol.name, nvol.attr['Used Size'], nvol.attr['Total Physical Used Size']))
 
