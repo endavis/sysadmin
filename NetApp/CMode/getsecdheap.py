eb03a58ed064de4efea950f555fc59e25dda8b55
Eric Davis <endavis@gmail.com>
2021-09-13 18:11:08 -0400

    add script to set heap

---
+++ NetApp/CMode/getsecdheap.py
@@ -0,0 +1,97 @@
+#/usr/bin/env python3
+from __future__ import division
+from Cluster import ClusterManager, naparser
+import sys
+import os
+import pprint
+from email.mime.text import MIMEText
+import smtplib
+from datetime import datetime
+
+
+fromaddress = 'SECDMONITORING@wolterskluwer.com'
+toaddress = 'eric.davis@wolterskluwer.com'
+
+def getdestination(cluster):
+  """
+  get information for a specific cluster
+  """
+  cmdnode1 = "set d;diag secd echo -node %s-01 -echo-text showLimits" % cluster.name
+  cmdnode2 = "set d;diag secd echo -node %s-02 -echo-text showLimits" % cluster.name
+
+  output = cluster.runinteractivecmd(cmdnode1)                                  
+  
+  for line in output:
+    if 'Max Virtual size' in line:
+      maxvsize = int(line.split()[-1])
+    elif 'Virtual Size' in line:
+      vsize = int(line.split()[-1])
+
+  percentage = '{:.2%}'.format(vsize / maxvsize)
+ 
+  now = datetime.now()
+  date_t = now.strftime('%d/%m/%Y %H:%M')
+
+  body = \
+"""
+Cluster: %s
+Node: %s
+Virtual Size: %s
+Max Virtual Size: %s
+Percentage: %s
+""" % (cluster.name, cluster.name + '-01', vsize, maxvsize, percentage)
+
+  output = cluster.runinteractivecmd(cmdnode2)                                  
+  
+  for line in output:
+    if 'Max Virtual size' in line:
+      maxvsize = int(line.split()[-1])
+    elif 'Virtual Size' in line:
+      vsize = int(line.split()[-1])
+
+  percentage = '{:.2%}'.format(vsize / maxvsize)
+ 
+  now = datetime.now()
+  date_t = now.strftime('%d/%m/%Y %H:%M')
+
+  body = body + \
+"""
+Cluster: %s
+Node: %s
+Virtual Size: %s
+Max Virtual Size: %s
+Percentage: %s
+""" % (cluster.name, cluster.name + '-01', vsize, maxvsize, percentage)
+
+  return body
+
+if __name__ == '__main__':
+  args = naparser.parse_args()
+
+  CLMan = ClusterManager(args)
+
+  now = datetime.now()
+  date_t = now.strftime('%d/%m/%Y %H:%M')
+
+  msgtext = """
+SECD Virtual Memory Update: %s
+
+  """ % date_t
+
+  for cluster in CLMan.clusters.values():
+      msgtext = msgtext + '\n' + getdestination(cluster)
+
+  print(msgtext)
+
+  msg = MIMEText(msgtext)
+
+  msg['Subject'] = 'SECD Virtual Memory Update: %s' % date_t
+  msg['From'] = fromaddress
+  msg['To'] = toaddress
+
+  s = smtplib.SMTP('localhost')
+  s.sendmail(fromaddress, toaddress, msg.as_string)
+  s.quit()
+
+
+    
