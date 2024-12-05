e1d7939245d9d42c6b7955842163d6253420c08b
Eric Davis <6662995+endavis@users.noreply.github.com>
2024-12-05 11:09:26 +0000

    update for new config

--- NetApp/API/cluster_usage.py
+++ NetApp/API/cluster_usage.py
@@ -12,7 +12,7 @@ import pprint
 
 #from workbook import SpaceWorkbook
 from netapp_ontap import HostConnection, utils
-from netapp_ontap.resources import Disk, Cluster, Volume, Node
+from netapp_ontap.resources import Disk, Cluster, Volume, Node, Aggregate
 
 from libs.config import Config
 from libs.parseargs import argp
@@ -33,36 +33,22 @@ class AppClass:
         self.build_app()
         self.vol_output_file = open('vol-out.csv', 'w')
         self.vol_output_file.write(",".join(['Cluster',
-                                             'Bucket',
                                              "App",
+                                             "Sub App",
                                              "Environment",
                                              "Tags",
                                              'Volume',
                                              'Type',
-                                             "Size (TiB)",
-                                             "Used (TiB)",
-                                             "Percent Used",
-                                             "Size if volume was 80% used (TiB)",
-                                             "Savings (TiB)",
+                                             "User Data Space Used (TiB)",
                                         "\n"])
                                 )   
         self.clus_output_file = open('cluster-out.csv', 'w')
         self.clus_output_file.write(",".join(['Cluster',
                                               "App",
+                                              "Sub App", 
                                               "Environment",
                                               "Tags",
-                                              "CVO - RW Provisioned Size (TiB)",
-                                              "CVO - RW Potential Savings (TiB)",
-                                              "CVO - RW Provisioned Size - Potential Savings",
-                                              "CVO - DP Provisioned Size (TiB)",
-                                              "CVO - DP Potential Savings (TiB)",
-                                              "CVO - DP Provisioned Size - Potential Savings",
-                                              "CVO HA - RW Provisioned Size (TiB)",
-                                              "CVO HA - RW Potential Savings (TiB)",
-                                              "CVO HA - RW Provisioned Size - Potential Savings",
-                                              "CVO HA - DP Provisioned Size (TiB)",
-                                              "CVO HA - DP Potential Savings (TiB)",
-                                              "CVO HA - DP Provisioned Size - Potential Savings",
+                                              "User Data Space Used (TiB)",
                                               "\n"])
                                 )   
 
@@ -82,87 +68,56 @@ class ClusterData:
             setattr(self, name, value)            
 
     def gather_data(self):
-        with HostConnection(self.ip, username=config.username, password=config.enc, verify=False):
+        with HostConnection(self.ip, username=config.settings['settings']['user']['name'], password=config.settings['settings']['user']['enc'], verify=False):
             cluster = Cluster()
             
             cluster.get()
 
             nodes = list(Node.get_collection(fields="ha"))
-            if len(nodes) > 1 and nodes[0]['ha']['enabled']:
-                self.cluster_type = 'CVO HA'
-            else:
-                self.cluster_type = 'CVO'
 
-            buckets = {'CVO HA - DP': {'provisioned_size': 0, 'potential_savings': 0},
-                       'CVO HA - RW': {'provisioned_size': 0, 'potential_savings': 0},
-                       'CVO - DP': {'provisioned_size': 0, 'potential_savings': 0},
-                       'CVO - RW': {'provisioned_size': 0, 'potential_savings': 0}}
+            total_user_data = 0
 
-            args = {}
-            args['is_svm_root'] = False
-            args['fields'] = ','.join(['name',
+            volume_args = {}
+            volume_args['is_svm_root'] = False
+            volume_args['fields'] = ','.join(['name',
                                       'size',
                                       'aggregates',
-                                      'space',
                                       'autosize.*',
                                       'guarantee',
                                       'space.*',
                                       'type',
                                     ])
 
-            volumes = list(Volume.get_collection(**args))
+            volumes = list(Volume.get_collection(**volume_args))
             for volume in volumes:
-                bucket = self.cluster_type + ' - ' + volume['type'].upper()
-                size_set_as_80 = 0
-                savings = 0
-                if volume['space']['percent_used'] < 80:
-                    size_set_as_80 = volume['space']['used'] * 1.2
-                    savings = volume['size'] - size_set_as_80
-                    if size_set_as_80 < 1073741824: # minimum size should be 1 GiB
-                        size_set_as_80 = 1073741824
-                else:
-                    size_set_as_80 = volume['size']
+                # if volume['name'] == "LTTY2022_CU010":
+                #      pprint.pprint(volume._last_state)
+                total_user_data += volume['space']['user_data']
                 APP.vol_output_file.write(",".join([self.name,
-                                                 bucket,                                                    
                                                  self.app,
+                                                 self.subapp,
                                                  self.env,
                                                  ";".join(self.tags), 
                                                 volume['name'],
                                                 volume['type'].upper(),
-                                                f"{approximate_size_specific(volume['size'], 'TiB', withsuffix=False)}",
-                                                f"{approximate_size_specific(volume['space']['used'], 'TiB', withsuffix=False)}",
-                                                f"{volume['space']['percent_used']}",
-                                                f"{approximate_size_specific(size_set_as_80, 'TiB', withsuffix=False)}",
-                                                f"{approximate_size_specific(savings, 'TiB', withsuffix=False)}",
+                                                f"{approximate_size_specific(volume['space']['user_data'], 'TiB', withsuffix=False)}",
                                                 "\n"])
                                         )
-                buckets[bucket]['provisioned_size'] += volume['size']
-                buckets[bucket]['potential_savings'] += savings
                 
             APP.clus_output_file.write(",".join([self.name,
                                                  self.app,
+                                                 self.subapp,
                                                  self.env,
                                                  ";".join(self.tags), 
-                                                 f"{approximate_size_specific(buckets['CVO - RW']['provisioned_size'], 'TiB', withsuffix=False)}",
-                                                 f"{approximate_size_specific(buckets['CVO - RW']['potential_savings'], 'TiB', withsuffix=False)}",
-                                                 f"{approximate_size_specific(buckets['CVO - RW']['provisioned_size'] - buckets['CVO - RW']['potential_savings'], 'TiB', withsuffix=False)}",                                                
-                                                 f"{approximate_size_specific(buckets['CVO - DP']['provisioned_size'], 'TiB', withsuffix=False)}",
-                                                 f"{approximate_size_specific(buckets['CVO - DP']['potential_savings'], 'TiB', withsuffix=False)}",
-                                                 f"{approximate_size_specific(buckets['CVO - DP']['provisioned_size'] - buckets['CVO - DP']['potential_savings'], 'TiB', withsuffix=False)}",   
-                                                 f"{approximate_size_specific(buckets['CVO HA - RW']['provisioned_size'], 'TiB', withsuffix=False)}",
-                                                 f"{approximate_size_specific(buckets['CVO HA - RW']['potential_savings'], 'TiB', withsuffix=False)}",
-                                                 f"{approximate_size_specific(buckets['CVO HA - RW']['provisioned_size'] - buckets['CVO HA - RW']['potential_savings'], 'TiB', withsuffix=False)}",                                                
-                                                 f"{approximate_size_specific(buckets['CVO HA - DP']['provisioned_size'], 'TiB', withsuffix=False)}",
-                                                 f"{approximate_size_specific(buckets['CVO HA - DP']['potential_savings'], 'TiB', withsuffix=False)}",
-                                                 f"{approximate_size_specific(buckets['CVO HA - DP']['provisioned_size'] - buckets['CVO HA - DP']['potential_savings'], 'TiB', withsuffix=False)}",   
+                                                 f"{approximate_size_specific(total_user_data, 'TiB', withsuffix=False)}",
                                                 "\n"])
                                         )
 
 if __name__ == '__main__':
     args = argp(description="gather volume and cluster stats, provisioned size and savings if changing to 80% and 90% autosize thresholds")
-    config = Config(args.config)
+    config = Config(args.data_dir, debug=False)
 
-    items = config.search(args.filters)
+    items = config.get_clusters(args.filters)
 
     APP = AppClass('Provisioned', items)
     APP.gather_data()
