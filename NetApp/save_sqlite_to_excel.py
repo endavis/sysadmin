1a4c03cab6598efff6146f6c48bd60d84bd5f543
Eric Davis <6662995+endavis@users.noreply.github.com>
2025-09-17 14:08:58 +0000

    now can pass tablename as an argument

--- NetApp/save_sqlite_to_excel.py
+++ NetApp/save_sqlite_to_excel.py
@@ -12,12 +12,13 @@ setup_logger()
 
 script_name = pathlib.Path(__file__).stem
 
-def save_database(database_file, output_file):
+def save_database(database_file, table_name, output_file):
+    logging.info
     # Connect to your SQLite database
     conn = sqlite3.connect(database_file)
 
     # Read the table into a pandas DataFrame
-    df = pd.read_sql_query("SELECT * FROM maintenance_events", conn)
+    df = pd.read_sql_query(f"SELECT * FROM {table_name}", conn)
 
     # Export to Excel
     df.to_excel(output_file, index=False, engine='xlsxwriter')  # or use engine='xlsxwriter'
@@ -25,9 +26,11 @@ def save_database(database_file, output_file):
 
 if __name__ == '__main__':
     args = argp(script_name=script_name ,description="gather volume and cluster stats, provisioned size and savings if changing to 80% and 90% autosize thresholds", parse=False)
-    args.parser.add_argument('-i', '--inputfile', type=str, help="inputfile", default="", required=True)
+    args.parser.add_argument('-i', '--inputfile', type=str, help="sqlite db file", default="", required=True)
+    args.parser.add_argument('-t', '--tablename', type=str, help="table name", default="", required=True)
+
     args.parse()
 
     config = Config(args.config_dir, args.output_dir)
 
-    save_database(args.inputfile, f'{args.output_dir}/azevents.xlsx')
+    save_database(args.inputfile, args.tablename, f'{args.output_dir}/azevents.xlsx')
