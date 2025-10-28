2f1a87af0c44f9b2befb7e4e4e1fc3999e4e7fce
Eric Davis <6662995+endavis@users.noreply.github.com>
2025-10-28 15:38:38 +0000

    black, ruff, and docstring updates

--- NetApp/libs/parseargs.py
+++ NetApp/libs/parseargs.py
@@ -1,3 +1,4 @@
+"""Module to parse command line arguments and set specific flags."""
 
 import argparse
 import json
@@ -6,36 +7,79 @@ import logging
 
 file_name = pathlib.Path(__file__).name
 
-class argp:
-    def __init__(self, script_name='unknown', description="default description", parse=True):
 
+class argp:
+    """Wraps argparse to add default arguments."""
+
+    def __init__(
+        self, script_name="unknown", description="default description", parse=True
+    ):
+        """Initialize the parser with common arguments."""
         self.description = description
 
         self.parser = argparse.ArgumentParser(description=self.description)
-        self.parser.add_argument('-c', '--config_dir', type=str, help="configuration directory", default='config')
-        self.parser.add_argument('-f', '--filter', type=str, help="""filter: Example: -f '{"bu":"Business", "env":"Prod", "tags":"active"}'""")
-        self.parser.add_argument('-de', '--debug', type=bool, default=False, help="""turn on debugging, default False""")
-        self.parser.add_argument('-o', '--output_dir', type=str, help="output directory", default=f"output/{script_name}")
+        self.parser.add_argument(
+            "-c",
+            "--config_dir",
+            type=str,
+            help="configuration directory",
+            default="config",
+        )
+        self.parser.add_argument(
+            "-f",
+            "--filter",
+            type=str,
+            help="""filter: Example: -f '{"bu":"Business","""
+            """ "env":"Prod", "tags":"active"}'""",
+        )
+        self.parser.add_argument(
+            "-de",
+            "--debug",
+            type=bool,
+            default=False,
+            help="""turn on debugging, default False""",
+        )
+        self.parser.add_argument(
+            "-o",
+            "--output_dir",
+            type=str,
+            help="output directory",
+            default=f"output/{script_name}",
+        )
 
         if parse:
             self.parse()
 
     def parse(self):
+        """Parse the arguments and set the filter and debug flag."""
         self.args = self.parser.parse_args(namespace=self)
 
-        if self.args.filter: # pyright: ignore[reportAttributeAccessIssue]
-            logging.debug(f"{file_name} : filter before conversion: {self.args.filter = }") # pyright: ignore[reportAttributeAccessIssue]
-            converted_filter = self.parse_json(self.args.filter) # pyright: ignore[reportAttributeAccessIssue]
-            logging.debug(f"{file_name} : After conversion: {type(converted_filter) = } {converted_filter = }")
-            self.args.filter = converted_filter # pyright: ignore[reportAttributeAccessIssue]
+        if self.args.filter:  # pyright: ignore[reportAttributeAccessIssue]
+            logging.debug(
+                f"{file_name} : filter before conversion: {self.args.filter = }"  # pyright: ignore[reportAttributeAccessIssue]
+            )
+            converted_filter = self.parse_json(
+                self.args.filter  # pyright: ignore[reportAttributeAccessIssue]
+            )
+            logging.debug(
+                f"{file_name} : After conversion: {type(converted_filter) = }"
+                f" {converted_filter = }"
+            )
+            self.args.filter = (  # pyright: ignore[reportAttributeAccessIssue]
+                converted_filter
+            )
         else:
-            self.args.filter = '' # pyright: ignore[reportAttributeAccessIssue]
+            self.args.filter = ""  # pyright: ignore[reportAttributeAccessIssue]
 
-        if self.args.debug: # pyright: ignore[reportAttributeAccessIssue]
+        if self.args.debug:  # pyright: ignore[reportAttributeAccessIssue]
             for handler in logging.getLogger().handlers:
                 handler.setLevel(logging.DEBUG)
 
     def parse_json(self, json_string):
+        """Parse a json string.
+
+        :param json_string: The json string to parse
+        """
         try:
             # Convert the JSON string to a dictionary
             arg_dict = json.loads(json_string)
@@ -44,4 +88,3 @@ class argp:
             logging.error(f"{file_name} : Error decoding JSON: {e}")
 
         return None
-
